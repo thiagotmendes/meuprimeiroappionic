9bc2995e0338840618a5472ab9a42dbf1e1233af
thiagotmendes <thiagotmendes@outloo.com>
2016-11-08 15:23:40 -0200

    inciando com ionic

---
+++ src/pages/home/home.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  public feeds: Array<string>;
+  private url: string = "https://www.reddit.com/new.json";
+
+  constructor(public navCtrl: NavController, public http: Http) {
+
+    this.http.get(this.url).map(res => res.json())
+      .subscribe(data => {
+        this.feeds = data.data.children;
+      });
+  }
+
+}
